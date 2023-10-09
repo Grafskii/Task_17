a9d29a388acb5ae715b59e59e94858ecbe262602
Grafskii <miti-h@yandex.ru>
2023-10-09 21:38:29 +0300

    two commit

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -25,6 +25,7 @@ public class PostmanEchoTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
